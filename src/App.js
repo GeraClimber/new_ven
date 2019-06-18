2c5a227ead5e8001968b19309672adfe28ba5cdf
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-06-18 21:07:39 +0300

    initial settings - upload images, fonts, begin header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './resources/styles.css'
 
+import Header from './components/header/Header'
 
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar
+                  position = "fixed"
+                  style = {{
+                      backgroundColor:'#2f2f2f',
+                      boxShadow: 'none',
+                      padding: '10px 0px'
+                  }}
+                >
+                    <Toolbar>
+                      <div className="header_logo">
+                          <div className="font_righteous header_logo_venue">The Venue</div>
+                          <div className="header_logo_title">Musical Events</div>
+                      </div>
+                      <Button color="inherit">Login</Button>
+                      <IconButton edge="start" style={{ marginLeft: 2}} color="inherit" aria-label="Menu">
+                          <MenuIcon />
+                      </IconButton>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
